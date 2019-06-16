9610a00132820066b1bc9f329933d112cffe004b
Vinayak Bhor <vinayak@Vinayaks-Mac.local>
2019-06-15 18:40:29 -0700

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Vinayak Bhor on 15/06/19.
+//  Copyright © 2019 Vinayak Bhor. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
